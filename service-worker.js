9820d6f40ba8fb17f35bc8f16310bd1770e6133d
Shinichi Okada <okada.shin@gmail.com>
2023-01-25 06:37:09 +0900

    fix: service-worker.js to es6

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
 // Change this to your repository name
-var GHPATH = '/svelte-awesome-icons';
+const GHPATH = '/svelte-awesome-icons';
 
 // Choose a different app prefix name
-var APP_PREFIX = 'svelte_awesome_icons_';
+const APP_PREFIX = 'svelte_awesome_icons_';
 
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_03';
+const VERSION = 'version_04';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
-var URLS = [
+const URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   '//cdn.jsdelivr.net/npm/docsify/lib/themes/vue.css',
