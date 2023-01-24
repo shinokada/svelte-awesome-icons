51fa83467c4a25a72a52bc58180c211119a74166
Shinichi Okada <okada.shin@gmail.com>
2023-01-25 06:35:25 +0900

    fix: file name and update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,13 @@
 var GHPATH = '/svelte-awesome-icons';
 
 // Choose a different app prefix name
-var APP_PREFIX = 'gppwa_';
+var APP_PREFIX = 'svelte_awesome_icons_';
 
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_02';
+var VERSION = 'version_03';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
