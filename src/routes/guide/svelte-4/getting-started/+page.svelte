9226950c7935a00f8be8e704bf53b6ca36558935
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-19 20:52:22 +0200

    docs: add bg-gray-50 to +layout Sidebar asideClass and aria-label updates

--- src/routes/guide/svelte-4/getting-started/+page.svelte
+++ src/routes/guide/svelte-4/getting-started/+page.svelte
@@ -12,22 +12,22 @@
 <h1>Getting Started - Svelte Awesome Icons v1</h1>
 
 <div class="my-8 flex gap-2">
-  <a href="https://github.com/sponsors/shinokada" target="_blank"
+  <a href="https://github.com/sponsors/shinokada" target="_blank" rel="nofollow" aria-label="Sponsor me on GitHub"
     ><img
       src="https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86"
       alt="sponsor"
     /></a
   >
-  <a href="https://www.npmjs.com/package/svelte-awesome-icons" rel="nofollow" target="_blank"
+  <a href="https://www.npmjs.com/package/svelte-awesome-icons" rel="nofollow" target="_blank" aria-label="npm svelte-awesome-icons"
     ><img src="https://img.shields.io/npm/v/svelte-awesome-icons" alt="npm" /></a
   >
-  <a href="https://opensource.org/licenses/MIT" rel="nofollow" target="_blank"
+  <a href="https://opensource.org/licenses/MIT" rel="nofollow" target="_blank" aria-label="MIT License"
     ><img
       src="https://img.shields.io/github/license/shinokada/svelte-awesome-icons"
       alt="License"
     /></a
   >
-  <a href="https://www.npmjs.com/package/svelte-awesome-icons" rel="nofollow" target="_blank"
+  <a href="https://www.npmjs.com/package/svelte-awesome-icons" rel="nofollow" target="_blank" aria-label="npm svelte-awesome-icons"
     ><img src="https://img.shields.io/npm/dw/svelte-awesome-icons.svg" alt="npm" /></a
   >
 </div>
