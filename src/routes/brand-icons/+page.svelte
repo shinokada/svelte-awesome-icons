e4ca85029cb686a20abc2f7f7c2c66209ca4ddfb
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-18 18:34:06 +0200

    fix: change svelte-5-ui-lib with flowbite-svelte

--- src/routes/brand-icons/+page.svelte
+++ src/routes/brand-icons/+page.svelte
@@ -9,5 +9,5 @@
   icons={keyIcons}
   title="Brand Icons - Svelte Awesome Icons"
   tab1Class="md:grid-cols-2 lg:grid-cols-3"
-  contentClass="rounded-lg dark_bg_theme mt-4"
+  contentClass="rounded-lg dark-bg-theme mt-4"
 />
--- src/routes/guide/svelte-5/props/+page.svelte
+++ src/routes/guide/svelte-5/props/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { HighlightCompo, CodeWrapper, Code, H2, H3 } from 'runes-webkit';
   import { BellRegular } from '$lib';
-  import { A, P } from 'svelte-5-ui-lib';
+  import { A, P } from 'flowbite-svelte';
 
   const modules = import.meta.glob('./md/*.md', {
     query: '?raw',
@@ -21,11 +21,11 @@
   >.
 </P>
 
-<HighlightCompo codeLang="ts" code={modules['./md/props.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/props.md'] as string} clipboardClass="top-2" />
 
 <H2>Types</H2>
 
-<HighlightCompo codeLang="ts" code={modules['./md/types.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/types.md'] as string} clipboardClass="top-2" />
 
 <H2>Size</H2>
 
@@ -33,7 +33,7 @@
   To change the size of an icon, use the <Code>size</Code> prop and specify the desired size. For example:
 </p>
 
-<HighlightCompo codeLang="ts" code={modules['./md/size.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/size.md'] as string} clipboardClass="top-2" />
 
 <p>
   You can add a custom size using Tailwind CSS by including the desired classes in the <Code
@@ -41,13 +41,13 @@
   > prop. For example:
 </p>
 
-<HighlightCompo codeLang="ts" code={modules['./md/size-2.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/size-2.md'] as string} clipboardClass="top-2" />
 
 <H2>CSS HEX Colors</H2>
 
 <p>Use the color attribute to change colors with HEX color code:</p>
 
-<HighlightCompo codeLang="ts" code={modules['./md/css-hex-color.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/css-hex-color.md'] as string} clipboardClass="top-2" />
 
 <H2>CSS framework</H2>
 
@@ -58,11 +58,11 @@
 
 <H3>Tailwind CSS</H3>
 
-<HighlightCompo codeLang="ts" code={modules['./md/tailwind-css.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/tailwind-css.md'] as string} clipboardClass="top-2" />
 
 <H3>Bootstrap</H3>
 
-<HighlightCompo codeLang="ts" code={modules['./md/bootstrap.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/bootstrap.md'] as string} clipboardClass="top-2" />
 
 <H2>Dark mode</H2>
 
@@ -72,7 +72,7 @@
   > prop.
 </p>
 
-<HighlightCompo codeLang="ts" code={modules['./md/dark-mode.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/dark-mode.md'] as string} clipboardClass="top-2" />
 
 <H2>A11y</H2>
 
@@ -82,13 +82,13 @@
   >. Use <Code>ariaLabel</Code> prop to modify the <Code>aria-label</Code> value.
 </p>
 
-<HighlightCompo codeLang="ts" code={modules['./md/a11y.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/a11y.md'] as string} clipboardClass="top-2" />
 
 <p>
   Use <Code>title</Code>, <Code>desc</Code>, and <Code>ariaLabel</Code> props to make your icons accessible.
 </p>
 
-<HighlightCompo codeLang="ts" code={modules['./md/a11y-2.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/a11y-2.md'] as string} clipboardClass="top-2" />
 
 <CodeWrapper>
   <BellRegular
@@ -116,4 +116,4 @@
     onclick={() => alert('hello')}
   />
 </CodeWrapper>
-<HighlightCompo codeLang="ts" code={modules['./md/passing-down-other-attributes.md'] as string} />
+<HighlightCompo codeLang="ts" code={modules['./md/passing-down-other-attributes.md'] as string} clipboardClass="top-2" />
